2816bb14322c141d8f42327ffadba5625f420bde
JackHaL <80892772+JackHaL-bit@users.noreply.github.com>
2024-03-24 20:01:03 +0300

    Update ethereum.sh

--- ethereum.sh
+++ ethereum.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-RPC_URL="https://eth1.lava.build/lava-referer-8b51600b-b3cc-4893-89ec-5de41f0724db/"
+RPC_URL="https://eth1.lava.build/lava-referer-fa29489a-c12b-4531-a087-8306decd03dc/"
 
 OUTPUT_DIR="logs"
 
